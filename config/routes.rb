bf4714e7c49179c080281416ed35d8269a9d225b
Niranjan Singh <niranjan987@gmail.com>
2014-10-14 00:14:19 -0700

    Add Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Getlinks::Application.routes.draw do
+  devise_for :users
   get "welcome/index"
   get "welcome/about"
   # The priority is based upon order of creation: first created -> highest priority.
